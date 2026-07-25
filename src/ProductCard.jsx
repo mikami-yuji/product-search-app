49ac9ff5e572110ae5e95c14fc3fd7915bd23a37
Your <you@example.com>
2026-07-25 16:26:00 +0900

    fix: support all order number and product code property variants in ProductCard.jsx

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -28,8 +28,8 @@ const ProductCard = ({ product, dirHandle, imageFilesMap, customerFileName, onCl
                 <ProductImage
                     dirHandle={dirHandle}
                     imageFilesMap={imageFilesMap}
-                    filename={product['受注№']}
-                    productCode={product['商品コード']}
+                    filename={product['受注№'] || product['受注No'] || product['受注番号'] || product['№'] || product['No']}
+                    productCode={product['商品コード'] || product['商品CD'] || product['コード']}
                     customerFileName={customerFileName}
                     className="amazon-card-image"
                 />
