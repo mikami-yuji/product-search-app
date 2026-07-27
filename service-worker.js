9c26f34bb6efaec6fc20acd943f734704c24460b
mikami-yuji <asahi.mikami54@gmail.com>
2026-07-27 00:18:49 +0000

    Deploying to gh-pages from @ mikami-yuji/product-search-app@5f791cdc842cf79d817884504eae1bcc3a799198 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker with Network-First strategy for app assets
-const CACHE_NAME = 'blazing-andromeda-cache-v15';
-const IMAGE_CACHE = 'blazing-andromeda-images-v15';
+const CACHE_NAME = 'blazing-andromeda-cache-v18';
+const IMAGE_CACHE = 'blazing-andromeda-images-v18';
 
 const PRECACHE_URLS = [
     './',
